a5f6524e376814089ccb446c2ad0e64e798e0a42
Yangwei <1318991632@qq.com>
2024-08-19 23:36:16 +0800

    修改Push，增加命令行输入

--- PushAuto.sh
+++ PushAuto.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# 远程仓库
+# gitee   git@gitee.com:yangwei1999/clearning.git (fetch)
+# gitee   git@gitee.com:yangwei1999/clearning.git (push)
+# github  git@github.com:Yangwei1999/Clearning.git (fetch)
+# github  git@github.com:Yangwei1999/Clearning.git (push)
 # 读取位置参数
 param1=$1
 
