1b8f0e204f54249a50ff0bfee74138cbe2da430c
Yangwei <1318991632@qq.com>
2024-08-19 23:33:01 +0800

    修改Push，增加命令行输入

--- PushAuto.sh
+++ PushAuto.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
+# 读取位置参数
+param1=$1
+
+# 打印读取的参数
+echo "提交信息为: $param1"
 
 git add .
 
-git commit -m "auto commit"
+git commit -m "$param1"
 
 echo "正在推送更改到远程仓库gitee..."
 git push -u gitee main
@@ -9,4 +16,4 @@ git push -u gitee main
 echo "正在推送更改到远程仓库github..."
 git push -u github main
 
-echo "推送已完成“
+echo "推送已完成：$param1“
